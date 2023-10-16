230a99460862fb66e4d619c017f710d52adb6349
vetakanfeta <148095232+vetakanfeta@users.noreply.github.com>
2023-10-16 09:52:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 import streamlit as st
+import numpy as np
+
+st.title("Echo")
 
 with st.chat_message("user"):
     st.write("Hello 👋")
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Say something"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
